42eba221b92ae206a823cbd07fce37966d562997
TranLuan2907 <nkluantran2907@gmail.com>
2023-07-14 20:35:44 +0700

    first commit

---
+++ src/com/fu/mathutil/MathUtil.java
@@ -0,0 +1,25 @@
+package com.fu.mathutil;
+
+public class MathUtil {
+
+    public static void main(String[] args) {
+
+    }
+    
+    public static long getFactorial(int n) {
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("Invalid Argument: n must be between 0 and 20");
+        }
+        
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        
+        long product = 1;
+        for (int i = 2; i <= n; i++) {
+            product *= i;
+        }
+        return product;
+    }
+
+}
---
+++ src/com/fu/mathutil/MathUtilMain.java
@@ -0,0 +1,11 @@
+package com.fu.mathutil;
+
+public class MathUtilMain {
+
+    public static void main(String[] args) {
+        long result = MathUtil.getFactorial(5);
+        System.out.println("expected: 5! = 120; actual:  " + result);
+        System.out.println("expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
+        System.out.println("expected 0! = 1; actual: " + MathUtil.getFactorial(0));
+    }
+}
